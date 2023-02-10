4b48e4f4fd1ea7aa899d61035ce035a4925f3b09
artem_levin <tema.levin2016@gmail.com>
2023-02-10 11:30:35 +0300

    Hello maria

---
+++ Examples005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша")
+{
+   Console.WriteLine("Привет Мария,давно не виделись");
+}
+else
+{
+   Console.WriteLine("Здраствуйте");
+   Console.Write(username);
+}
