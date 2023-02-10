0c0de5d24dddb56614277833945b33cb24250ee3
artem_levin <tema.levin2016@gmail.com>
2023-02-10 11:13:04 +0300

    random

--- Exampels003_sum/Program.cs
+++ Exampels003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1, 10); //1 2 3 4...9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
